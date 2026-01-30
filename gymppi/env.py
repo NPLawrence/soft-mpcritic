a873c196c7bcf471b689babb5ebf54083054fc8c
tbanker <thomas_banker@berkeley.edu>
2026-01-30 15:41:55 -0800

    batched mppi & wrappers

--- gymppi/env.py
+++ gymppi/env.py
@@ -3,41 +3,47 @@ from copy import deepcopy
 from typing import TypeVar
 import numpy as np
 
-# reference:
-# https://github.com/Farama-Foundation/HighwayEnv/blob/master/highway_env/envs/common/abstract.py
-# BaseEnvWrapper used for actual environment the agent interacts with
-# - has state attribute for reliable MPPI
-# MMPIEnvWrapper used for simulating MPPI rollouts
-# - has unique step to prevent rollout termination/truncation state initialization
-# - has unique reset to set initial state for reliable MPPI
-
 Observation = TypeVar("Observation")
 
 class BaseEnvWrapper(gymnasium.Wrapper):
+    """base environment that the agent interacts with online"""
     def __init__(self, env):
         gymnasium.Wrapper.__init__(self, env)
 
     @property
-    def _state(self):
-        if 'classic' in self.env.unwrapped.spec.entry_point:
-            return self.env.unwrapped.state
-        elif 'mujoco' in self.env.unwrapped.spec.entry_point:
-            state = self.env.unwrapped.state_vector()
-            ... # other environments beyond DIP may need more items to fully describe state
+    def _extra(self):
+        """extra stuff not in observations necessary to represent the full system state"""
+        if 'mujoco' in self.env.unwrapped.spec.entry_point:
+            if 'swimmer' in self.env.unwrapped.spec.entry_point:
+                return {'x_position':self.env.unwrapped.data.qpos[[0]],
+                        'y_position':self.env.unwrapped.data.qpos[[1]]}
             if 'reacher' in self.env.unwrapped.spec.entry_point:
-                state = np.concat([state, self.env.unwrapped.goal])
-            return state
-        elif 'highway' in self.env.unwrapped.spec.entry_point:
-            return self.env.unwrapped.road
+                return {'goal_position':self.env.unwrapped.data.qpos[-2:],
+                        'goal_velocity':self.env.unwrapped.data.qvel[-2:]}
 
-class MPPIEnvWrapper(gymnasium.Wrapper):
+
+class ClassicMPPIWrapper(gymnasium.Wrapper):
+    """environment used for MPPI rollouts for classic control environments"""
     def __init__(self, env):
         gymnasium.Wrapper.__init__(self, env)
+        self.env_id = env.unwrapped.spec.id
 
     def step(self, action):
         obs, reward, terminated, truncated, info = super().step(action)
-        terminated, truncated = False, False
+        terminated, truncated = False, False # don't stop MPPI rollouts early
         return obs, reward, terminated, truncated, info
+    
+    def obs2state(self, observation):
+        if self.env_id == 'Pendulum-v1':
+            cos_theta = observation[...,[0]]
+            sin_theta = observation[...,[1]]
+            theta_dot = observation[...,[2]]
+            theta = np.arctan2(sin_theta, cos_theta)
+            state = np.concatenate([theta, theta_dot], axis=-1)
+        elif self.env_id == 'MountainCarContinuous-v0':
+            state = observation
+
+        return state
 
     def reset(
         self,
@@ -48,40 +54,89 @@ class MPPIEnvWrapper(gymnasium.Wrapper):
 
         obs, info = super().reset(seed=seed, options=options)
 
-        if (options is not None) and ('classic' in self.env.unwrapped.spec.entry_point):
-            self.env.unwrapped.state = deepcopy(options['state'])
-            try: # Pendulum-v1
-                obs = self.env.unwrapped._get_obs()
-            except: # MountainCar
-                obs = self.env.unwrapped.state
-            info = {}
-        elif (options is not None) and ('mujoco' in self.env.unwrapped.spec.entry_point):
-            n_qpos = np.prod(self.env.unwrapped.data.qpos.shape)
-            n_qvel = np.prod(self.env.unwrapped.data.qvel.shape)
-            self.env.unwrapped.data.qpos = deepcopy(options['state'][:n_qpos])
-            self.env.unwrapped.data.qvel = deepcopy(options['state'][n_qpos:n_qpos+n_qvel])
-            ... # other environments may need more items
-            if 'reacher' in self.env.unwrapped.spec.entry_point:
-                self.env.unwrapped.goal = options['state'][n_qpos+n_qvel:]
-            obs = self.env.unwrapped._get_obs()
-            info = self.env.unwrapped._get_reset_info()
-        if (options is not None) and ("highway" in self.env.unwrapped.spec.entry_point):
-            # under development...
-            self.env.unwrapped.define_spaces()  # First, to set the controlled vehicle class depending on action space
-            self.env.unwrapped.time = self.env.unwrapped.steps = 0
-            self.env.unwrapped.done = False
-            road = deepcopy(options['state'])
-            # vs = road.vehicles
-            # keys = road.vehicles[0].__dict__.keys()
-            # for i, v in enumerate(self.env.unwrapped.road.vehicles):
-            #     for k in keys:
-            #         setattr(v, k, getattr(vs[i], k))
-            self.env.unwrapped.road = road
-            self.env.unwrapped.define_spaces()
-            obs = self.env.unwrapped.observation_type.observe()
-            # print(obs)
-            info = self.env.unwrapped._info(obs, action=self.env.unwrapped.action_space.sample())
-            if self.env.unwrapped.render_mode == "human":
-                self.render()
+        if options.get('observation') is not None:
+            state = self.obs2state(options.get('observation'))
+        self.env.unwrapped.state = deepcopy(state)
+
+        if self.env_id == 'Pendulum-v1':
+            observation = self.env.unwrapped._get_obs()
+        elif self.env_id == 'MountainCarContinuous-v0':
+            observation = self.env.unwrapped.state
+
+        info = {}
+
+        return observation, info
+
+
+class MujocoMPPIWrapper(gymnasium.Wrapper):
+    """environment used for MPPI rollouts for Mujoco environments"""
+    def __init__(self, env):
+        gymnasium.Wrapper.__init__(self, env)
+        self.env_id = self.env.unwrapped.spec.id
+        self.n_qpos = np.prod(self.env.unwrapped.data.qpos.shape)
+        self.n_qvel = np.prod(self.env.unwrapped.data.qvel.shape)
+
+    def step(self, action):
+        obs, reward, terminated, truncated, info = super().step(action)
+        terminated, truncated = False, False # don't stop MPPI rollouts early
+        return obs, reward, terminated, truncated, info
+
+    def obs2state(self, observation, extra=None):
+        if self.env_id == 'InvertedPendulum-v5':
+            qpos = observation[:self.n_qpos]
+            qvel = observation[self.n_qpos:self.n_qpos+self.n_qvel]
+
+        if self.env_id == 'InvertedDoublePendulum-v5':
+            cart_pos = observation[[0]]
+            sin_theta1 = observation[[1]]
+            sin_theta2 = observation[[2]]
+            cos_theta1 = observation[[3]]
+            cos_theta2 = observation[[4]]
+            theta1 = np.arctan2(sin_theta1, cos_theta1)
+            theta2 = np.arctan2(sin_theta2, cos_theta2)
+            qpos = np.concatenate([cart_pos, theta1, theta2], axis=-1)
+            qvel = observation[5:5+self.n_qvel]
+
+        if self.env_id == 'Swimmer-v5':
+            qpos = np.concatenate([extra['x_position'], extra['y_position'], observation[:3]])
+            qvel = observation[3:3+self.n_qvel]
+
+        if self.env_id == 'Reacher-v5':
+            cos_theta1 = observation[[0]]
+            cos_theta2 = observation[[1]]
+            sin_theta1 = observation[[2]]
+            sin_theta2 = observation[[3]]
+            theta1 = np.arctan2(sin_theta1, cos_theta1)
+            theta2 = np.arctan2(sin_theta2, cos_theta2)
+            qpos = np.concatenate([theta1, theta2, extra['goal_position']])
+            qvel = np.concatenate([observation[6:8], extra['goal_velocity']])
+
+        return qpos, qvel
+
+    def reset(
+        self,
+        *,
+        seed: int | None = None,
+        options: dict | None = None,
+    ) -> tuple[Observation, dict]:
+
+        obs, info = super().reset(seed=seed, options=options)
+
+        if options.get('extra') is not None:
+            extra = deepcopy(options.get('extra'))
+        else:
+            extra = None
+
+        if options.get('observation') is not None:
+            qpos, qvel = self.obs2state(options.get('observation'), extra)
+
+        self.env.unwrapped.set_state(deepcopy(qpos), deepcopy(qvel))
+
+        if self.env_id == 'reacher':
+            self.env.unwrapped.goal = deepcopy(extra['goal_position'])
+        ... # other environments may need more items
+
+        obs = self.env.unwrapped._get_obs()
+        info = self.env.unwrapped._get_reset_info()
 
         return obs, info
--- gymppi/mppi.py
+++ gymppi/mppi.py
@@ -7,7 +7,7 @@ from torch.distributions import MultivariateNormal
 # https://github.com/UM-ARM-Lab/pytorch_mppi/blob/master/src/pytorch_mppi/mppi.py
 
 class MPPI():
-    def __init__(self, env, rollout_envs=None, l=None, f=None, Q=None, T=10, K=100, mu=None, cov=None, lambda_=1.0, u_init=None, U_init=None, u_scale=None):
+    def __init__(self, env, rollout_envs=None, l=None, f=None, Q=None, T=10, K=100, N=1, mu=None, cov=None, lambda_=1.0, u_init=None, U_init=None):
         self.env = env
         self.rollout_envs = rollout_envs
         self.env_dtype = self.env.observation_space.dtype
@@ -22,6 +22,7 @@ class MPPI():
         self.f = f # dynamics
         self.Q = Q # terminal value function (assumes we want u^* = argmin -Q(s,a))
 
+        self.N = N # number of state inputs/particles
         self.K = K # number of trajectory samples
         self.T = T # length of trajectories/horizon
 
@@ -31,7 +32,7 @@ class MPPI():
 
         self.lambda_ = lambda_ # free energy scale/parameter
         self.mu = torch.zeros(self.nu, dtype=self.dtype) if (mu is None) else mu # mean of action noise distribution
-        self.cov = torch.diag(torch.ones(self.nu, dtype=self.dtype)) if (cov is None) else cov # covariance matrix of action noise distribution
+        self.cov = torch.diag(torch.ones(self.nu, dtype=self.dtype)) if (cov is None) else cov.to(self.dtype) # covariance matrix of action noise distribution
         self.inv_cov = torch.inverse(self.cov)
         self.noise_dist = MultivariateNormal(self.mu, covariance_matrix=self.cov)
 
@@ -45,16 +46,14 @@ class MPPI():
         self.cost_total = None
         self.cost_total_non_zero = None
         self.omega = None
-        self.observations = None
+        self.rollout_observation = None
+        self.rollout_observations = None
         self.actions = None
 
+    @torch.no_grad()
     def make_step(self, observation):
         """return action for given observation"""
-        self._sync_envs()
-
-        if not torch.is_tensor(observation):
-            observation = torch.tensor(observation)
-        self.observation = observation.to(dtype=self.dtype)
+        self.observation = torch.tensor(observation).to(dtype=self.dtype).view(self.N, self.ns)
 
         # sample \epsilon ~ N(mu, cov)
         noise = self.noise_dist.rsample((self.K, self.T+1))
@@ -65,12 +64,22 @@ class MPPI():
         # reflect action constraints in noise
         noise = self.perturbed_action - self.U
 
-
         action_cost = self.lambda_ * noise @ self.inv_cov # (24) inner summation
-
-        rollout_cost = self._compute_rollout_costs(self.perturbed_action)
-
         perturbation_cost = torch.sum(self.U * action_cost, dim=(1, 2)) # (24) inner summation
+
+        # repeating to account for number of state inputs/particles
+        noise = noise.repeat(self.N, 1, 1)
+        perturbation_cost = perturbation_cost.repeat(self.N)
+
+        # calculate NxK costs
+        rollout_costs = []
+        for i in range(self.N):
+            self.rollout_observation = self.observation[i]
+            self._sync_envs(self.rollout_observation.numpy())
+            rollout_costs.append(self._compute_rollout_costs(self.rollout_observation, self.perturbed_action))
+
+        rollout_cost = torch.cat(rollout_costs, dim=0)
+
         cost_total = rollout_cost + perturbation_cost # (24) inner sum
 
         beta = torch.min(cost_total) # "ensure that at least one trajectory has non-zero mass"
@@ -84,40 +93,39 @@ class MPPI():
         action = self.U[0]
         return action.numpy()
     
-    def _compute_rollout_costs(self, perturbed_action):
-        """compute cost/reward for trajectory"""
+    def _compute_rollout_costs(self, rollout_observation, perturbed_action):
+        """compute cost/reward for K trajectories"""
         rollout_cost = torch.zeros(self.K, dtype=self.dtype)
 
-        observation = self.observation.view(1, -1).repeat(self.K, 1)
+        observation = rollout_observation.repeat(self.K, 1)
         observations = [observation]
         actions = []
 
-        with torch.no_grad():
-            for t in range(self.T):
-                action = perturbed_action[:, t]
+        for t in range(self.T):
+            action = perturbed_action[:, t]
 
-                next_observation, l = self._step_rollout(observation, action)
-                rollout_cost += l
+            next_observation, l = self._step_rollout(observation, action)
+            rollout_cost += l
 
-                observation = next_observation
+            observation = next_observation
 
-                actions.append(action)
-                observations.append(observation)
+            actions.append(action)
+            observations.append(observation)
 
-            if self.Q is not None:
-                action = perturbed_action[:, self.T+1]
-                rollout_cost += -self.Q(observation, action).flatten()
+        if self.Q is not None:
+            action = perturbed_action[:, self.T+1]
+            rollout_cost += -self.Q(observation, action).flatten()
 
-                next_observation, _ = self._step_rollout(observation, action)
-                observation = next_observation
+            next_observation, _ = self._step_rollout(observation, action)
+            observation = next_observation
 
-                actions.append(action)
-                observations.append(observation)
+            actions.append(action)
+            observations.append(observation)
 
         # Actions is K x T+1 x nu
         # Observations is K x T+2 x nx
         self.actions = torch.stack(actions, dim=-2)
-        self.observations = torch.stack(observations, dim=-2)
+        self.rollout_observations = torch.stack(observations, dim=-2)
 
         return rollout_cost
     
@@ -132,23 +140,52 @@ class MPPI():
         if self.f is not None:
             return self.f(observation, action), self.l(observation, action).flatten()
         else:
-            next_obsveration, rewards, terminations, truncations, infos = self.rollout_envs.step(action.numpy())
-            return torch.from_numpy(next_obsveration).view(-1, self.ns), -torch.from_numpy(rewards)
+            next_observation, rewards, terminations, truncations, infos = self.rollout_envs.step(action.numpy())
+            return torch.from_numpy(next_observation).view(self.K, self.ns), -torch.from_numpy(rewards)
 
-    def _sync_envs(self):
-        """align the """
+    def _sync_envs(self, observation):
+        """align the mppi environments"""
         if self.rollout_envs:
-            state = self.env.env._state # access BaseEnv state
-            observation, _ = self.rollout_envs.reset(options={'state':state}) # set MPPIEnvs to state
-            self.mppi_observation = observation
+            self.rollout_envs.reset(options={'observation':observation,
+                                             'extra':self.env.get_wrapper_attr('_extra')}) # set MPPIEnvs to state
 
     def reset(self, U_init=None):
         """reinitialize all MPPI computations"""
         self.U = self.noise_dist.sample((self.T+1,)) if (U_init is None) else U_init
+        self.rollout_observation = None
         self.observation = None
         self.info = None
         self.cost_total = None
         self.cost_total_non_zero = None
         self.omega = None
-        self.observations = None
+        self.rollout_observations = None
         self.actions = None
+
+if __name__ == '__main__':
+    import gymnasium as gym
+    from networks import StageCost, Dynamics
+    from env import BaseEnvWrapper, ClassicMPPIWrapper, MujocoMPPIWrapper
+
+    N = 5
+    K = 500
+
+    env = BaseEnvWrapper(gym.make('Reacher-v5', render_mode='human', width=720, height=720))
+    rollout_envs = gym.make_vec('Reacher-v5', num_envs=K, vectorization_mode="sync", wrappers=[MujocoMPPIWrapper])
+
+    cov = 0.01*torch.diag(torch.ones(np.prod(env.action_space.shape)))
+    mppi = MPPI(env, rollout_envs=rollout_envs, K=K, T=20, N=N, cov=cov)
+
+    obs, _ = env.reset()
+    env.render()
+    
+    for _ in range(500):
+        belief = np.repeat([obs], N, axis=0)
+        belief = belief + 1.*np.random.randn(*belief.shape)
+        action = mppi.make_step(belief)
+            
+        obs, _, _, _, _ = env.step(action)
+        print(action)
+        env.render()
+
+    env.close()
+    rollout_envs.close()
