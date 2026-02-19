5c2c6a54a52200892e9ceebab8c6583814046114
tbanker <thomas_banker@berkeley.edu>
2026-02-18 16:48:27 -0800

    batched particle MPPI

--- gymppi/mppi.py
+++ gymppi/mppi.py
@@ -6,8 +6,10 @@ from torch.distributions import MultivariateNormal
 # https://doi.org/10.1109/ICRA.2017.7989202 "Information theoretic MPC for model-based reinforcement learning"
 # https://github.com/UM-ARM-Lab/pytorch_mppi/blob/master/src/pytorch_mppi/mppi.py
 
+
+
 class MPPI():
-    def __init__(self, env, rollout_envs=None, l=None, f=None, Q=None, T=10, K=100, N=1, mu=None, cov=None, lambda_=1.0, u_init=None, U_init=None):
+    def __init__(self, env, rollout_envs=None, l=None, f=None, Q=None, mu=None, B=1, P=1, T=10, K=100, mean=None, cov=None, lambda_=1.0, u_init=None):
         self.env = env
         self.rollout_envs = rollout_envs
         self.env_dtype = self.env.observation_space.dtype
@@ -21,8 +23,10 @@ class MPPI():
         self.l = l # stage cost
         self.f = f # dynamics
         self.Q = Q # terminal value function (assumes we want u^* = argmin -Q(s,a))
+        self.mu = mu # controller
 
-        self.N = N # number of state inputs/particles
+        self.B = B # batch size
+        self.P = P # number of particles
         self.K = K # number of trajectory samples
         self.T = T # length of trajectories/horizon
 
@@ -31,13 +35,14 @@ class MPPI():
             assert self.rollout_envs.num_envs == self.K # one rollout_env for each sample
 
         self.lambda_ = lambda_ # free energy scale/parameter
-        self.mu = torch.zeros(self.nu, dtype=self.dtype) if (mu is None) else mu # mean of action noise distribution
+        self.mean = torch.zeros(self.nu, dtype=self.dtype) if (mean is None) else mean # mean of action noise distribution
         self.cov = torch.diag(torch.ones(self.nu, dtype=self.dtype)) if (cov is None) else cov.to(self.dtype) # covariance matrix of action noise distribution
         self.inv_cov = torch.inverse(self.cov)
-        self.noise_dist = MultivariateNormal(self.mu, covariance_matrix=self.cov)
+        self.noise_dist = MultivariateNormal(self.mean, covariance_matrix=self.cov)
 
-        self.U = self.noise_dist.sample((self.T+1,)) if (U_init is None) else U_init # initial trajectory
-        self.u_init = torch.zeros_like(self.mu) if (u_init is None) else u_init # initialization for last action of trajectory at each step
+        self.U = self.noise_dist.sample((self.B, 1, self.T+1,)) # .repeat(1, self.P, 1, 1) # initial action sequence shared amongst particles
+        self.noise = torch.zeros(self.B, self.K, self.T+1, self.nu, dtype=self.dtype)
+        self.u_init = torch.zeros_like(self.mean) if (u_init is None) else u_init # initialization for last action of trajectory at each step
         
         self.observation = None
         self.info = None
@@ -53,47 +58,50 @@ class MPPI():
     @torch.no_grad()
     def make_step(self, observation):
         """return action for given observation"""
-        self.observation = torch.tensor(observation).to(dtype=self.dtype).view(self.N, self.ns)
+        self.observation = torch.tensor(observation).to(dtype=self.dtype).view(self.B, self.P, self.ns)
 
-        # sample \epsilon ~ N(mu, cov)
-        noise = self.noise_dist.rsample((self.K, self.T+1))
-        # v = u + \epsilon; broadcast U to noise over samples; now it's K x T+1 x nu
-        perturbed_action = self.U + noise
-        # ensure actions are within action space
-        self.perturbed_action = self._bound_action(perturbed_action)
-        # reflect action constraints in noise
-        noise = self.perturbed_action - self.U
+        # initialize action sequence with previous solution
+        self.U = torch.roll(self.U, -1, dims=1)
+        self.U[:,-1] = self.u_init
 
-        action_cost = self.lambda_ * noise @ self.inv_cov # (24) inner summation
-        perturbation_cost = torch.sum(self.U * action_cost, dim=(1, 2)) # (24) inner summation
+        # calculate batch B actions where each of B is computed across P particles for K noise sequences
+        for b in range(self.B):
+            self.b = b
 
-        # repeating to account for number of state inputs/particles
-        noise = noise.repeat(self.N, 1, 1)
-        perturbation_cost = perturbation_cost.repeat(self.N)
+            # sample \epsilon ~ N(mean, cov)
+            self.noise[b] = self.noise_dist.rsample((self.K, self.T+1))
 
-        # calculate NxK costs
-        rollout_costs = []
-        for i in range(self.N):
-            self.rollout_observation = self.observation[i]
-            self._sync_envs(self.rollout_observation.numpy())
-            rollout_costs.append(self._compute_rollout_costs(self.rollout_observation, self.perturbed_action))
+            # simulate actions + noise over particles to calculate P x K costs
+            rollout_costs = []
+            for p in range(self.P):
+                self.p = p
 
-        rollout_cost = torch.cat(rollout_costs, dim=0)
+                self.rollout_observation = self.observation[b,p]
+                self._sync_envs(self.rollout_observation.numpy())
+                rollout_costs.append(self._compute_rollout_costs(self.rollout_observation))
 
-        cost_total = rollout_cost + perturbation_cost # (24) inner sum
+            rollout_cost = torch.cat(rollout_costs, dim=0)
+            action_cost = self.lambda_ * self.noise[b] @ self.inv_cov # (24) inner summation
+            perturbation_cost = torch.sum(self.U[b] * action_cost, dim=(1, 2)) # (24) inner summation
 
-        beta = torch.min(cost_total) # "ensure that at least one trajectory has non-zero mass"
-        cost_total_non_zero = torch.exp(-self.lambda_ * (cost_total - beta)) # (24) exp
-        eta = torch.sum(cost_total_non_zero) # (25)
-        omega = (1. / eta) * cost_total_non_zero # (24) normalize for sample weights
+            # repeating cost for across P particles because noise is shared
+            perturbation_cost = perturbation_cost.repeat(self.P)
+            rep_noise = self.noise[b].repeat(self.P,1,1)
 
-        perturbations = torch.sum(omega.view(-1, 1, 1) * noise, dim=0) # (26) summation
-        self.U = self.U + perturbations # (26)
+            cost_total = rollout_cost + perturbation_cost # (24) inner sum
 
-        action = self.U[0]
+            beta = torch.min(cost_total) # "ensure that at least one trajectory has non-zero mass"
+            cost_total_non_zero = torch.exp(-self.lambda_ * (cost_total - beta)) # (24) exp
+            eta = torch.sum(cost_total_non_zero) # (25)
+            omega = (1. / eta) * cost_total_non_zero # (24) normalize for sample weights
+
+            perturbations = torch.sum(omega.view(-1, 1, 1) * rep_noise, dim=0) # (26) summation
+            self.U[b] = self.U[b] + perturbations # (26)
+
+        action = self.U[:,0]
         return action.numpy()
     
-    def _compute_rollout_costs(self, rollout_observation, perturbed_action):
+    def _compute_rollout_costs(self, rollout_observation):
         """compute cost/reward for K trajectories"""
         rollout_cost = torch.zeros(self.K, dtype=self.dtype)
 
@@ -102,7 +110,7 @@ class MPPI():
         actions = []
 
         for t in range(self.T):
-            action = perturbed_action[:, t]
+            action = self._get_perturbed_action(observation, t)
 
             next_observation, l = self._step_rollout(observation, action)
             rollout_cost += l
@@ -113,7 +121,7 @@ class MPPI():
             observations.append(observation)
 
         if self.Q is not None:
-            action = perturbed_action[:, self.T+1]
+            action = self._get_perturbed_action(observation, self.T+1)
             rollout_cost += -self.Q(observation, action).flatten()
 
             next_observation, _ = self._step_rollout(observation, action)
@@ -122,13 +130,26 @@ class MPPI():
             actions.append(action)
             observations.append(observation)
 
-        # Actions is K x T+1 x nu
-        # Observations is K x T+2 x nx
+        # Actions is K x T x nu or K x T+1 x nu if self.Q
+        # Observations is K x T+1 x nx or K x T+2 x ns if self.Q
         self.actions = torch.stack(actions, dim=-2)
         self.rollout_observations = torch.stack(observations, dim=-2)
 
         return rollout_cost
     
+    def _get_perturbed_action(self, observation, t):
+        if self.mu:
+            self.U[self.b, :, t] = self.mu(observation)
+        
+        # v = u + \epsilon; broadcast U to noise over noise; now it's K x nu
+        perturbed_action = self.U[self.b, :, t] + self.noise[self.b, :, t] # noise shared among particles
+        # ensure actions are within action space
+        perturbed_action = self._bound_action(perturbed_action)
+        # reflect action constraints in noise
+        self.noise[self.b, :, t] = perturbed_action - self.U[self.b, :, t]
+
+        return perturbed_action
+
     def _bound_action(self, action):
         """bound action within action space"""
         if self.u_max is not None:
@@ -160,30 +181,33 @@ class MPPI():
         self.omega = None
         self.rollout_observations = None
         self.actions = None
+        self.noise = None
 
 if __name__ == '__main__':
     import gymnasium as gym
     from networks import StageCost, Dynamics
     from env import BaseEnvWrapper, ClassicMPPIWrapper, MujocoMPPIWrapper
 
-    N = 5
+    B = 3
+    P = 2
     K = 500
 
-    env = BaseEnvWrapper(gym.make('Reacher-v5', render_mode='human', width=720, height=720))
-    rollout_envs = gym.make_vec('Reacher-v5', num_envs=K, vectorization_mode="sync", wrappers=[MujocoMPPIWrapper])
+    env = BaseEnvWrapper(gym.make('InvertedPendulum-v5', render_mode='human'))
+    ns = env.observation_space.shape[0]
+    rollout_envs = gym.make_vec('InvertedPendulum-v5', num_envs=K, vectorization_mode="sync", wrappers=[MujocoMPPIWrapper])
 
-    cov = 0.01*torch.diag(torch.ones(np.prod(env.action_space.shape)))
-    mppi = MPPI(env, rollout_envs=rollout_envs, K=K, T=20, N=N, cov=cov)
+    cov = 1*torch.diag(torch.ones(np.prod(env.action_space.shape)))
+    mppi = MPPI(env, rollout_envs=rollout_envs, K=K, T=20, B=B, P=P, cov=cov)
 
     obs, _ = env.reset()
     env.render()
     
     for _ in range(500):
-        belief = np.repeat([obs], N, axis=0)
-        belief = belief + 1.*np.random.randn(*belief.shape)
+        batch = np.repeat([obs[None,:]], B, axis=0)
+        belief = batch + 0.*np.random.randn(B,P,ns)
         action = mppi.make_step(belief)
             
-        obs, _, _, _, _ = env.step(action)
+        obs, _, _, _, _ = env.step(action[0,0])
         print(action)
         env.render()
 
