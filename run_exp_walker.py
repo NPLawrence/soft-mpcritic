62ebec27a45d46e6b9ce92d3f580a2fbdc21a07a
tbanker <thomas_banker@berkeley.edu>
2026-03-26 10:12:02 -0700

    walker config

--- run_exp_walker.py
+++ run_exp_walker.py
@@ -3,7 +3,7 @@ from ddpg_continuous_action import Args, train
 if __name__ == "__main__":
     args = Args()
     args.wandb_entity = "mpcritic-dpc"
-    args.wandb_project_name = "dual_mpcritic_ddpg_new_value_with_discount_iterations_walker"
+    args.wandb_project_name = "dual_mpcritic_ddpg_terminations_walker"
     args.track = True
     args.save_model = True
     args.env_in_mppi = False
@@ -11,18 +11,20 @@ if __name__ == "__main__":
 
     # agreed upon
     args.total_timesteps = 500000
-    args.batch_size = 64
-    args.horizon = 3
+    args.batch_size = 32
+    args.horizon = 4
     args.num_rollouts = 200 # 200 (previous experiments) or 500 (30% slower)
     args.num_target_rollouts = int(args.num_rollouts // 10)
     args.var = 0.1 # 0.1 (previous experiments) or 0.05 (seems to actually train on walker)
     args.lambda_ = 0.1
-
-    args.transition_network = "small_deep"
+    args.transition_network = "medium"
     args.transition_ensemble_size = None
+    args.mppi_handle_terminations = True
 
+    args.mppi_prior = 'gaussian'
+    args.mppi_prior = 'uniform'
     for seed in range(5):
         args.seed = seed
-        for mppi_online_iterations in [1,2,4]:
-            args.mppi_online_iterations = mppi_online_iterations
+        for distributional_dynamics in [False, True]:
+            args.distributional_dynamics = distributional_dynamics
             train(args)
